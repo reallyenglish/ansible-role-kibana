bc600d80501f91647f150ca75c4dac1aea7eaaf5
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-04-11 11:41:19 +0900

    add tests

---
+++ spec/serverspec/default_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'serverspec'
+
+kibana_package_name = 'kibana'
+kibana_service_name = 'kibana'
+kibana_config_path  = '/etc'
+kibana_user_name    = 'kibana'
+kibana_user_group   = 'kibana'
+
+case os[:family]
+when 'freebsd'
+  kibana_package_name = 'kibana45'
+  kibana_config_path = '/usr/local/etc'
+end
+
+describe package(kibana_package_name) do
+  it { should be_installed }
+end
+
+describe file("#{kibana_config_path}/kibana.yml") do
+  it { should be_file }
+  its(:content) { should match /^server\.port: 5601/ } 
+  its(:content) { should match /^server\.host: 0\.0\.0\.0/ }
+  its(:content) { should match /^elasticsearch\.url: "http:\/\/localhost:9200"/ } 
+  its(:content) { should match /^kibana\.index: "\.kibana"/ }
+  its(:content) { should match /^logging\.dest: "stdout"/ }
+end
+
+describe port(5601) do
+  it { should be_listening }
+end
+
+describe service(kibana_service_name) do
+  it { should be_enabled }
+  it { should be_running }
+end
