3be7672af89f14817c16425d7a24f4be63d5c621
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-04-12 08:54:42 +0900

    wait for kibana to start listening in spec

--- spec/serverspec/default_spec.rb
+++ spec/serverspec/default_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 require 'serverspec'
 
+# waits for kibana to start listening. in my test environment, it takes around
+# 40 sec. without this, 'kitchen test' would fail.
+#
+# {"type":"log","@timestamp":"2016-04-11T23:38:36+00:00","tags":["info","optimize"],"pid":1029,"message":"Optimizing and caching bundles for kibana and statusPage. This may take a few minutes"}
+# {"type":"log","@timestamp":"2016-04-11T23:39:17+00:00","tags":["info","optimize"],"pid":1029,"message":"Optimization of bundles for kibana and statusPage complete in 40.45 seconds"}
+# ...
+# {"type":"log","@timestamp":"2016-04-11T23:39:17+00:00","tags":["listening","info"],"pid":1029,"message":"Server running at http://0.0.0.0:5601"}
+
+sleep 50
+
 kibana_package_name = 'kibana'
 kibana_service_name = 'kibana'
 kibana_config_path  = '/etc'
@@ -26,11 +36,11 @@ describe file("#{kibana_config_path}/kibana.yml") do
   its(:content) { should match /^logging\.dest: "stdout"/ }
 end
 
-describe port(5601) do
-  it { should be_listening }
-end
-
 describe service(kibana_service_name) do
   it { should be_enabled }
   it { should be_running }
+end
+
+describe port(5601) do
+  it { should be_listening }
 end
