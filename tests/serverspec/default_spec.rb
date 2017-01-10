f23b15a7efb06232d65f2d335f5e279eda22a47a
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-01-10 16:25:49 +0900

    refactor

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,23 +1,23 @@
-require 'spec_helper'
-require 'serverspec'
+require "spec_helper"
+require "serverspec"
 
-kibana_package_name = 'kibana'
-kibana_service_name = 'kibana'
-kibana_config_path  = '/etc'
-kibana_user_name    = 'kibana'
-kibana_user_group   = 'kibana'
+kibana_package_name = "kibana"
+kibana_service_name = "kibana"
+kibana_config_path  = "/etc/kibana.yml"
+kibana_user_name    = "kibana"
+kibana_user_group   = "kibana"
 
 case os[:family]
-when 'freebsd'
-  kibana_package_name = 'kibana45'
-  kibana_config_path = '/usr/local/etc'
+when "freebsd"
+  kibana_package_name = "kibana45"
+  kibana_config_path = "/usr/local/etc/kibana.yml"
 end
 
 describe package(kibana_package_name) do
   it { should be_installed }
 end
 
-describe file("#{kibana_config_path}/kibana.yml") do
+describe file(kibana_config_path) do
   it { should be_file }
   its(:content_as_yaml) { should include("server.port" => 5601) }
   its(:content_as_yaml) { should include("server.host" => "0.0.0.0") }
